ed26fdf8e910a9cebb6df851ee08fc5d1ed93b39
dtele <72906211+dtele@users.noreply.github.com>
2021-07-19 06:36:04 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+import json
+from os import getcwd
+import sys
+
+import discord
+from discord.ext import commands
+from reactionmenu import ButtonsMenu, ComponentsButton
+
+cogs_to_path = [sys.path.append(rf'{getcwd()}\cogs\{i}') for i in os.listdir(rf'{getcwd()}\cogs')]
+
+from filterhelp import FilterHelp
+from m_steam import Steam
+from webster import Dictionary
+
+
+with open('preferences.json', 'r') as file:
+    keys = json.load(file)
+
+prefix = keys["prefix"]
+bot = commands.Bot(command_prefix=prefix)
+ButtonsMenu.initialize(bot=bot)
+
+bot.add_cog(Steam(bot, prefix))
+bot.add_cog(FilterHelp(bot, prefix))
+bot.add_cog(Dictionary(bot, prefix, keys["webster_key"]))
+
+bot.remove_command('help')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} online\nServers: {", ".join([i.name for i in list(bot.guilds)])}')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=keys["activity"]))
+
+
+bot.run(keys["discord_key"])
