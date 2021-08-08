21cbd993306e08218f19c142b01e334bb979d0de
dtele <72906211+dtele@users.noreply.github.com>
2021-08-08 08:48:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@
 import json
 import sys
 from os import getcwd, listdir
-from random import randrange
-from re import sub
 
 import discord
 from discord.ext import commands
@@ -15,6 +13,7 @@ cogs_to_path = [sys.path.append(rf'{getcwd()}\cogs\{i}') for i in listdir(rf'{ge
 from filterhelp import FilterHelp
 from m_steam import Steam
 from webster import Dictionary
+from tmdb import Tmdb
 
 
 with open('preferences.json', 'r') as file:
@@ -22,32 +21,28 @@ with open('preferences.json', 'r') as file:
 
 prefix = keys["prefix"]
 bot = commands.Bot(command_prefix=prefix)
-ButtonsMenu.initialize(bot=bot)
 
 bot.add_cog(Steam(bot, prefix))
 bot.add_cog(FilterHelp(bot, prefix))
 bot.add_cog(Dictionary(bot, prefix, keys["webster_key"]))
+bot.add_cog(Tmdb(bot, prefix, keys["tmdb_key"]))
 
 bot.remove_command('help')
-
-
-@bot.event
-async def on_message(msg):
-    ctx = await bot.get_context(msg)
-    text = sub('https?:\/\/\S+\.\w+\/\S+\/\S+', '', msg.content)
-    if not msg.author.bot:
-        if 'bruh' in text:
-            await ctx.send('bruh')
-        elif '69' in text:
-            await ctx.send(f'{":rofl:" * randrange(1, 4 + 1)} {"lo" * randrange(3, 8 + 1) + "l"} 69 {":thumbsup:" * randrange(1, 4 + 1)}')
-
-    await bot.process_commands(msg)
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} online\nServers: {", ".join([i.name for i in list(bot.guilds)])}')
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=keys["activity"]))
+
+
+@bot.event
+async def on_message(msg):
+    ctx = await bot.get_context(msg)
+    if 'bruh' in msg.content and not msg.author.bot:
+        await ctx.send('bruh')
+
+    await bot.process_commands(msg)
 
 
 bot.run(keys["discord_key"])
